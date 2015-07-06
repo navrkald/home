0a093dce35834267f3fdc73df504a292b9a887c8
David Navrkal <navrkald@centrum.cz>
2015-07-06 11:43:08 +0200

    Created deply scritp and README.

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# If occur some error end this cript
+set -e
+
+# Move all dirs to parrent dir
+find . -maxdepth 1 -type d ! -name '.' -exec mv {} .. \;
+# Move files exept this script to parrent dir
+find . -maxdepth 1 -type f ! -wholename "$0" -exec mv {} .. \;
+
+# Append .davebashrc to .bashrc
+echo "\n" >> ../.bashrc
+echo "if [ -e ../.dave_bashrc ]; then " >> ../.bashrc
+echo "  . .dave_bashrc" >> ../.bashrc
+echo "fi" >> ../.bashrc
+
+# Remove this script itself
+rm $0
